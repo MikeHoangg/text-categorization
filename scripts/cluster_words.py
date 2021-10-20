b97c2dc93309dd18924fa6be2e4caf3966e2ceec
mikehoang <hoang.viet.ha.business@gmail.com>
2021-10-20 21:40:51 +0300

    fix(clusterization): added optimal clusters number determnation

--- scripts/cluster_words.py
+++ scripts/cluster_words.py
@@ -1,34 +1,56 @@
 import os
 import argparse
+
 import pandas as pd
 import numpy as np
 
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from typing import Iterable
 
 from scripts import SPACY_PROCESSOR
 from main import run, OUTPUT_FOLDER, export
 
 
-def cluster_words(data: pd.DataFrame, num_of_clusters: int = 20) -> pd.DataFrame:
+def get_kmeans_cluster_labels(data: list, num_of_clusters: int):
+    """
+    Method for getting silhouette score and cluster labels
+    :param data: data for clustering
+    :param num_of_clusters: number of clusters
+    :return: tuple: number of clusters, score, cluster labels
+    """
+    kmeans_words = KMeans(n_clusters=num_of_clusters)
+    kmeans_words.fit(data)
+    new_cluster_labels = kmeans_words.labels_
+
+    return num_of_clusters, silhouette_score(data, new_cluster_labels), new_cluster_labels
+
+
+def cluster_words_kmeans(data: Iterable, num_of_clusters: int = 10) -> pd.DataFrame:
     """
     Function for clustering words into groups
-    :param data: DataFrame obj
+    :param data: iterable list or sequence of words
     :param num_of_clusters: number of clusters
     :return: DataFrame of clusters
     """
-    data = SPACY_PROCESSOR(' '.join(data['word']))
+    data = SPACY_PROCESSOR(' '.join(data))
     data = pd.DataFrame([(x.text, x.vector) for x in data], columns=['word', 'vector'])
-    kmeans_words = KMeans(n_clusters=num_of_clusters).fit(np.array([x for x in data['vector']]))
-    data['labels'] = kmeans_words.labels_
-    res = [names['word'].tolist() for label, names in data.groupby('labels')]
-    return pd.DataFrame([(cluster,) for cluster in res], columns=['cluster'])
+    vector_data = np.array([*data['vector']])
+
+    res = [get_kmeans_cluster_labels(vector_data, x) for x in range(2, num_of_clusters + 1)]
+
+    # assigning cluster for each word
+    data['cluster'] = max(res, key=lambda x: x[1])[2]
+    clusters = [df['word'].tolist() for label, df in data.groupby('cluster')]
+    return pd.DataFrame([(cluster,) for cluster in clusters], columns=['cluster'])
 
 
 @run
 def main():
     def read_args() -> argparse.Namespace:
         parser = argparse.ArgumentParser()
-        parser.add_argument('--num_cluster', '-n', help='Number of clusters', type=int, required=False)
+        parser.add_argument('--word_threshold', '-w', help='Word threshold for clusters', type=int, required=False,
+                            default=100)
         parser.add_argument('--input_path', '-i', help='Full path for input file', type=str, required=True)
         parser.add_argument('--output_path', '-o', help=f'Name for output file, it will be created in {OUTPUT_FOLDER}',
                             type=str, required=True)
@@ -41,7 +63,7 @@ def main():
         raise Exception('input file must have json extension')
 
     data = pd.read_json(args.input_path)
-    res = cluster_words(data, args.num_cluster)
+    res = cluster_words_kmeans(data['word'])
     export(res, os.path.join(OUTPUT_FOLDER, args.output_path))
 
 
